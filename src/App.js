8c24a180de6827ee610675c9092cf5adb4101fe0
uzunyusuf <yusufuzn19@gmail.com>
2021-01-06 14:07:32 +0300

    transfer file from other my portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import About from "./components/About";
+import Banner from "./components/Banner";
+import Contact from "./components/Contact";
+import Nav from "./components/Nav";
+import Portfolio from "./components/Portfolio";
+import Services from "./components/Services";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner />
+      <Nav />
+      <Services />
+      <About />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export default styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 250px;
+  width: 100%;
+  background-color: #1f2235;
+  color: #fff;
+  margin: 15px;
+  font-size: 15px;
+  
+`;

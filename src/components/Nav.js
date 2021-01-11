ed5abad9a32aae6b3c4b5388ef5d762d6896c973
uzunyusuf <yusufuzn19@gmail.com>
2021-01-11 18:45:49 +0300

    edit toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FaAlignJustify } from "react-icons/fa";
 
 const Nav = () => {
-  const [state, setState] = React.useState(false);
+  const [state, setState] = React.useState(true);
   return (
     <nav className="navbar">
       <div className="container">
